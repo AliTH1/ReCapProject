8dc42359a523bbea13dcceb8d692c48c26d70754
Ali <a.taghizada02@gmail.com>
2023-09-29 07:58:50 +0400

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+Car car = new Car();
+car.Id = 1;
+car.Description = "Ali";
+
+carManager.Add(car);
+
+
+foreach (var item in carManager.GetAll())
+{
+    Console.WriteLine(item.Description);
+}
